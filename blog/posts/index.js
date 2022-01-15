7c28550f88325f732588e3ad8fa0704e02b9913e
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-15 20:40:42 -0300

    feat: create posts service

---
+++ blog/posts/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(express.json())
+
+const port = 8080
+
+const posts = {}
+
+app.get("/posts", (request, response) => {
+  response.json(posts)
+})
+
+app.post('/posts', (request, response) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = request.body
+
+  posts[id] = { id, title }
+
+  response.status(201).json(posts[id])
+})
+
+app.listen(port, () => {
+  console.log(`Posts server is running on port: ${port}`)
+})
