de82dbc7370e4b39b2723acc64a6d465b2218372
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-15 22:12:16 -0300

    feat: create comments service

---
+++ blog/comments/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(express.json())
+
+const port = 8081
+
+const commentsByPostId = {}
+
+app.get("/posts/:id/comments", (request, response) => {
+  response.json(commentsByPostId[request.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (request, response) => {
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = request.body
+
+  const comments = commentsByPostId[request.params.id] || []
+
+  comments.push({ id: commentId, content })
+
+  commentsByPostId[request.params.id] = comments
+
+  response.status(201).json(comments)
+})
+
+app.listen(port, () => {
+  console.log(`Comments server is running on port: ${port}`)
+})
