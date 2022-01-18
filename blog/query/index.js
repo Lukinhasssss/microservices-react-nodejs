b1c562f975f6a4a7f99a0c4ccce0b930d9c78325
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-18 20:53:15 -0300

    feat: parse incoming events

--- blog/query/index.js
+++ blog/query/index.js
@@ -3,13 +3,36 @@ const cors = require("cors")
 
 const app = express()
 app.use(express.json())
-app.use(cors)
+app.use(cors())
 
 const port = 8082
 
-app.get("/posts", (request, response) => {})
+const posts = {}
 
-app.post("/events", (request, response) => {})
+app.get("/posts", (request, response) => {
+  response.json(posts)
+})
+
+app.post("/events", (request, response) => {
+  const { type, data } = request.body
+
+  if (type === 'PostCreated') {
+    const { id, title } = data
+
+    posts[id] = { id, title, comments: [] }
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data
+
+    const post = posts[postId]
+    post.comments.push({ id, content })
+  }
+
+  console.log(posts)
+
+  response.json({})
+})
 
 app.listen(port, () => {
   console.log(`Query server is running on port: ${port}`)
