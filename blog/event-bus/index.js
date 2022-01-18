a85ffc6efab6917e76dc3dbbd6be1900221506d9
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-17 21:10:39 -0300

    feat: add basic event bus implementation

---
+++ blog/event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const axios = require("axios")
+
+const app = express()
+app.use(express.json())
+
+const port = 8090
+
+app.post("/events", (request, response) => {
+  const event = request.body
+
+  axios.post("http://localhost:8000/events", event).catch(err => {
+    console.log(err.message)
+  })
+
+  axios.post("http://localhost:8081/events", event).catch(err => {
+    console.log(err.message)
+  })
+
+  axios.post("http://localhost:8082/events", event).catch(err => {
+    console.log(err.message)
+  })
+
+  response.json({ status: 'OK' })
+})
+
+app.listen(port, () => {
+  console.log(`Event-bus server is running on port: ${port}`)
+})
