9f37bc0a43001747b5dca768e59017a2b0e6321d
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-17 21:51:57 -0300

    fix: change endpoint name for axios request

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -9,7 +9,7 @@ const port = 8090
 app.post("/events", (request, response) => {
   const event = request.body
 
-  axios.post("http://localhost:8000/events", event).catch(err => {
+  axios.post("http://localhost:8080/events", event).catch(err => {
     console.log(err.message)
   })
 
