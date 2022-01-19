c2a3e148c6fda6e413683524ae3e7a994144eaff
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-18 21:07:52 -0300

    feat: use the query service on react app

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,19 +1,4 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([])
-
-  const fetchData = async () => {
-    const response = await axios.get(`http://localhost:8081/posts/${postId}/comments`)
-
-    setComments(response.data)
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     return <li key={ comment.id }>{ comment.content }</li>
   })
