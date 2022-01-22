93d759572a23a84116760211b7c97ddfd4e3165c
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-22 16:24:34 -0300

    feat: render comments by status

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,6 +1,17 @@
 const CommentList = ({ comments }) => {
-  const renderedComments = comments.map((comment) => {
-    return <li key={ comment.id }>{ comment.content }</li>
+  const renderedComments = comments.map(comment => {
+    let content
+
+    if (comment.status === 'APPROVED')
+      content = comment.content
+
+    if (comment.status === 'PENDING')
+      content = 'This comment is awaiting moderation'
+
+    if (comment.status === 'REJECTED')
+      content = 'This comment has been rejected'
+
+    return <li key={ comment.id }>{ content }</li>
   })
 
   return <ul>{ renderedComments }</ul>
