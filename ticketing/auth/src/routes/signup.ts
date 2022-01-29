d7f489a2c81891a9ef656b56899659d1c4499f55
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-29 17:30:05 -0300

    feat: add validations in signupRouter

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import { body } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/users/signup', (request, response) => {
-  response.json({ message: 'Hello World!' })
+router.post('/api/users/signup', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must have at least 8 characters'),
+], (request: Request, response: Response) => {
+  const { email, password } = request.body
+
+  
 })
 
 export { router as signupRouter }
