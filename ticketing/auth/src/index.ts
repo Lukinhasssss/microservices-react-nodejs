ca0bedef6449693e1af12834cccd90da4c72f4f8
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-29 17:01:28 -0300

    feat: create route handlers

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express'
+import { currentUserRouter } from './routes/current-user'
 
 const app = express()
 app.use(express.json())
 
 const port = 8080
 
-app.get('/api/users/currentuser', (request, response) => {
-  response.json({ message: 'Hello World!' })
-})
+app.use(currentUserRouter)
 
 app.listen(port, () => {
   console.log(`Auth server is running on port: ${port}`)
