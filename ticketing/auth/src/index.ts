c1dc5f19953d1de07157bfe1f5005d9f772146d4
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-01-29 16:48:54 -0300

    chore: add ingress-nginx config file

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -5,6 +5,10 @@ app.use(express.json())
 
 const port = 8080
 
+app.get('/api/users/currentuser', (request, response) => {
+  response.json({ message: 'Hello World!' })
+})
+
 app.listen(port, () => {
   console.log(`Auth server is running on port: ${port}`)
 })
