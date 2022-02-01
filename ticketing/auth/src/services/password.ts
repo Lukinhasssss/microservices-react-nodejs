f9ec102fe4dfb2e6792a815c5b3cde87876b9b93
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-02-01 20:22:24 -0300

    feat: compare hashed password

--- ticketing/auth/src/services/password.ts
+++ ticketing/auth/src/services/password.ts
@@ -11,8 +11,11 @@ class Password {
     return `${buffer.toString('hex')}.${salt}`
   }
 
-  static compare(storedPassword: string, suppliedPassword: string) {
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.') // hashedPassword is the hash password that is stored in the database, salt is the salt generated during the initial hashing process
+    const buffer = await scryptAsync(suppliedPassword, salt, 64) as Buffer
 
+    return buffer.toString('hex') === hashedPassword
   }
 }
 
