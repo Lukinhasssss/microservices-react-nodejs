f9bb198382759781c22a33bf845110549fae1896
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-02-19 10:24:05 -0300

    feat: return the current user

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (request, response) => {
-  response.json({ message: 'Hello World!' })
+  if (!request.session?.jwt)
+    return response.json({ currentUser: null })
+
+  try {
+    const payload = jwt.verify(request.session.jwt, process.env.JWT_KEY!)
+
+    return response.json({ currentUser: payload })
+  } catch (error) {
+    return response.json({ currentUser: null })
+  }
 })
 
 export { router as currentUserRouter }
