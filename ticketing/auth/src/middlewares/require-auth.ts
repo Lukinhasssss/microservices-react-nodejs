553327f2317b951f0050066e1be70b9f9dc458c5
Lucas Monteiro <lukasmonteir0@hotmail.com>
2022-04-22 11:07:05 -0300

    feat: add middleware for require auth for route access

---
+++ ticketing/auth/src/middlewares/require-auth.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express'
+import UnauthorizedError from '../errors/unauthorized-error'
+
+export const requireAuth = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (!request.currentUser)
+    throw new UnauthorizedError()
+
+  next()
+}
